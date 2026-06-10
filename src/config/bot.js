6e83be6b9c2ecab88aa90c98c46728d019e6f188
zr6cc <lundgaardmikkel2008@gmail.com>
2026-06-10 14:37:14 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "zr6c",
         // Activity type number (0 = Playing).
         type: 0, 
       },
